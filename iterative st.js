3c1f23ba7735be9ecd23e1aca1d55a624410812f
Prasadthungala7 <bhanugthungala@gmail.com>
2021-12-10 22:10:38 +0530

    hello

--- iterative st.js
+++ iterative st.js
@@ -1,11 +1,34 @@
-// for(let i=1; i>=10; i=i+i){
+// for(let i=100; i<=1; i=i-i){
 //     console.log(i);
-//     i=i+1;
 // }
 
 
-let i = 100;
-while(i <= 10){
+// let i = 100;
+// while(i >= 1){
+//     console.log(i);
+//     i = i-1;
+// }
+
+
+
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i = i + 1;
+// }
+
+
+// let a=49;
+// if (a%7==2){
+//     console.log("It is divisible by 7");
+// }else 
+// {
+//     console.log("It is not divisible by 7");
+// }
+
+
+let i=50;
+while(i>=1){
     console.log(i);
     i=i-1;
 }
