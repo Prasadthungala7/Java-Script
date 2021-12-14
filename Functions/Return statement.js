c9561ff844c9e2c0453db54ac75cfa2e354d306c
Prasadthungala7 <bhanugthungala@gmail.com>
2021-12-14 11:54:15 +0530

    Okay

---
+++ Functions/Return statement.js
@@ -0,0 +1,19 @@
+// function add(a, b){
+//     return a*b;
+// }
+// let x=add(20, 40);
+
+// console.log(x);
+
+
+
+
+
+//   function greet(message){
+//     message = message + 1000;
+//     //execute few statements
+//     return message;
+//     console.log("Hello, Good Night");
+//   }
+//   let x = greet("Good Morning");
+//   console.log(x);
---
+++ Functions/Without Parameter.js
@@ -0,0 +1,31 @@
+// //Function With any Parameter:
+
+// function add(a, b){   //Function Parameter(a, b)
+//     console.log(a+b);
+// }
+
+// add(10, 20);  //Function Arguments(10, 20)
+
+
+
+
+// function add(name, lastName){   //Function Parameter(a, b)
+//     console.log('Hello ' + name + '' + lastName);
+// }
+
+// add('Prasad', 'World');
+
+
+
+
+const name = "Prasad";
+
+function logger(){
+    console.log("Learning Time")
+    console.log("Learning Time")
+    console.log("Learning Time")
+    console.log("Learning Time")
+    console.log("Learning Time")
+}
+
+logger();
