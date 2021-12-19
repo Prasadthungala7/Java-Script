245d6567d567e290419cdab464483a25224828ed
Prasad <95238639+Prasadthungala7@users.noreply.github.com>
2021-12-19 22:36:51 +0530

    Update iterative st.js

--- iterative st.js
+++ iterative st.js
@@ -1,30 +1,32 @@
-// for(let i=100; i<=1; i=i-i){
-//     console.log(i);
-// }
+for(let i=100; i<=1; i=i-i){
+    console.log(i);
+}
 
 
-// let i = 100;
-// while(i >= 1){
-//     console.log(i);
-//     i = i-1;
-// }
+let i = 100;
+while(i >= 1){
+    console.log(i);
+    i = i-1;
+}
 
 
 
-// let i = 1;
-// while (i <= 10) {
-//   console.log(i);
-//   i = i + 1;
-// }
+let i = 1;
+while (i <= 10) {
+  console.log(i);
+  i = i + 1;
+}
 
 
-// let a=49;
-// if (a%7==2){
-//     console.log("It is divisible by 7");
-// }else 
-// {
-//     console.log("It is not divisible by 7");
-// }
+
+let a=49;
+if (a%7==2){
+    console.log("It is divisible by 7");
+}else 
+{
+    console.log("It is not divisible by 7");
+}
+
 
 
 let i=50;
